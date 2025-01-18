32caf62d9ee536bf128f1892a0cd3cf9a0cb5540
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-01-18 19:46:28 -0300

    chore: add lint-staged configuration for multiple file types in lint-staged.config.js

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+export default {
+    "*.html": ["eslint --fix", "prettier --write"],
+    "*.ts": ["eslint --fix", "prettier --write"],
+    "*.tsx": ["eslint --fix", "prettier --write"],
+    "*.js": ["eslint --fix", "prettier --write"],
+    "*.jsx": ["eslint --fix", "prettier --write"],
+    "*.css": ["eslint --fix", "prettier --write"],
+    "*.json": ["eslint --fix", "prettier --write"],
+};
